291a98ea63be567c3c186ad206a229c7c9cd3919
LittleFool <littlefool@fassiburg.de>
2020-05-14 14:39:23 +0200

    added classes Message and User

---
+++ ZNC Log Importer/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZNC_Log_Importer
+{
+    class Message
+    {
+        private DateTime time { get; }
+        private String mode { get; }
+        private String name { get; }
+        private String text { get; }
+
+        public Message(DateTime time, String mode, String name, String text)
+        {
+            this.time = time;
+            this.mode = mode;
+            this.name = name;
+            this.text = text;
+        }
+    }
+}
---
+++ ZNC Log Importer/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ZNC_Log_Importer
+{
+    class User
+    {
+        private string name;
+        private string mode;
+        private List<Message> messages = new List<Message>();
+
+        public User(string name, string mode)
+        {
+            this.name = name;
+            this.mode = mode;
+        }
+
+        public void setMode(DateTime time, string mode)
+        {
+            Message m = new Message(time, this.mode, name, "changed mode to " + mode);
+            messages.Add(m);
+            this.mode = mode;
+        }
+
+        public void setName(DateTime time, string name)
+        {
+            Message m = new Message(time, mode, this.name, "changed name to " + name);
+            messages.Add(m);
+            this.name = name;
+        }
+
+        public void addMessage(Message m)
+        {
+            messages.Add(m);
+        }
+    }
+}
